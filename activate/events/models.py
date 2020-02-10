454343089e023aa937dd6d1e536a04e171a0a3fc
Oda Omholt <oda.omh@gmail.com>
2020-02-10 13:16:14 +0100

    feat: create Activity model.

--- activate/events/models.py
+++ activate/events/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Activity(models.Model):
+    """Model for activities. Contains all necessary info for a single activity event."""
+    title = models.CharField(max_length=200, verbose_name="tittel")
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    #responsible = models.ForeignKey(User, .....)
+    date_from = models.DateTimeField(verbose_name="fra dato")
+    date_to = models.DateTimeField(verbose_name="til dato")
+    #registered_users = models.ManyToManyField(User, blank=True, verbose_name='påmeldte brukere')
+
+
+    def __str__(self):
+        return self.title
