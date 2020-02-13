369b12b989e97b5ea3a021b850fe55d911ffa05e
ingebjrgb <ingebjrgb@gmail.com>
2020-02-13 14:24:20 +0100

    feat: add activity_list function to events view

--- activate/events/views.py
+++ activate/events/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Activity
 
-# Create your views here.
+
+def activity_list(request):
+    activities = Activity.objects.all()
+    return render(request, 'events/activity_list.html', {'activities': activities})
