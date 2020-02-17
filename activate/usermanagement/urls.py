1801f9e4a6a6f5463be3ea5e882bc5781a6bff83
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-02-17 11:52:46 +0100

    feat: add signup view

--- activate/usermanagement/urls.py
+++ activate/usermanagement/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.view_login, name='view_login'),
     path('logout/', views.view_logout, name='view_logout'),
+    path('signup/', views.view_signup, name = 'view_signup')
 ]
--- activate/usermanagement/views.py
+++ activate/usermanagement/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -23,3 +23,23 @@ def view_logout(request):
     logout(request)
     response = redirect('/login/')
     return response
+
+def view_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.Post)
+        if form.is_valid():
+            # Create user
+            form.save()
+
+            # Manually authenticate the user
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            # Compare hashed and raw data
+            user = authenticate(username = username, password = raw_password)
+            # Securely log in the user
+            login(request, user)
+            return redirect('login_page.html')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
