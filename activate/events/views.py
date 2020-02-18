d8ca4d402a91e6e6620412ac93f93fb6ce4a7924
magvaag <magnusvaagenmorud@gmail.com>
2020-02-18 18:12:15 +0100

    feat: add create_activity response to form

--- activate/events/views.py
+++ activate/events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from events.forms import CreateActivityForm
 from .models import Activity
@@ -10,5 +10,20 @@ def activity_list(request):
 
 
 def create_activity(request):
-    form = CreateActivityForm()
+    # User is rejected if they are not logged in
+    if request.user is None or request.user.is_anonymous:
+        return activity_list(request)
+
+    if request.method == 'POST':
+        form = CreateActivityForm(request.POST)
+        if form.is_valid():
+            activity = form.save(commit=False)  # Create activity object, but do not commit it to the database just yet
+            activity.responsible = request.user  # Add the currently logged in user as 'responsible' for this activity
+            activity.save()  # Save the activity to the database
+
+            # TODO : We should probably redirect to the detailed activity page when successfully creating an activity
+            return redirect('/')  # Redirects to the home page
+    else:
+        form = CreateActivityForm()
+
     return render(request, 'events/activity_create.html', {'form': form})
