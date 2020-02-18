b05e85797dbb08e2049440d1818c3578515ebe52
magvaag <magnusvaagenmorud@gmail.com>
2020-02-18 18:07:53 +0100

    feat: add reference to model Activity in CreateActivityForm

--- activate/events/forms.py
+++ activate/events/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
+from events.models import Activity
 
-class CreateActivityForm(forms.Form):
+
+class CreateActivityForm(forms.ModelForm):
     title = forms.CharField(label='Tittel', max_length=200)
     text = forms.CharField(label='Beskrivelse', widget=forms.Textarea())
     date_from = forms.DateTimeField(label='Fra dato', help_text='Eks: 2012-04-18 11:01')
     date_to = forms.DateTimeField(label='Til dato', help_text='Eks: 2012-04-18 11:01')
+
+    class Meta:
+        model = Activity
+        fields = ('title', 'text', 'date_from', 'date_to')
