2c7fa592b2cc0d1390003220cada144e12f974f4
Sofieoj <sofie_99@outlook.com>
2020-02-20 13:05:34 +0100

    feat: Add gear field in CreateActivityForm

--- activate/events/forms.py
+++ activate/events/forms.py
@@ -8,7 +8,8 @@ class CreateActivityForm(forms.ModelForm):
     text = forms.CharField(label='Beskrivelse', widget=forms.Textarea())
     date_from = forms.DateTimeField(label='Fra dato', help_text='Eks: 2012-04-18 11:01')
     date_to = forms.DateTimeField(label='Til dato', help_text='Eks: 2012-04-18 11:01')
+    gear = forms.CharField(label='Utstyr', max_length=200, required=False, help_text='Optional')
 
     class Meta:
         model = Activity
-        fields = ('title', 'text', 'date_from', 'date_to')
+        fields = ('title', 'text', 'date_from', 'date_to', 'gear')
