a3bd6f1652c2c137ae4c409f9a437b4bc751770c
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-02-21 16:02:39 +0100

    feat: add birth date to signup form

--- activate/usermanagement/forms.py
+++ activate/usermanagement/forms.py
@@ -1,13 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+import datetime
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=False, help_text='Valgfri.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Valgfri.')
+    birth_date = forms.DateField(label='Fødselsdato',
+                                 widget=forms.SelectDateWidget
+                                 (years=[x for x in range(1970, datetime.datetime.now().year)]),
+                                 required=False, help_text='Valgfri.')
+    email = forms.EmailField(max_length=254, help_text='Påkrevet. Skriv inn en gyldig email-adresse.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+        fields = ('username', 'first_name', 'last_name', 'birth_date', 'email', 'password1', 'password2',)
