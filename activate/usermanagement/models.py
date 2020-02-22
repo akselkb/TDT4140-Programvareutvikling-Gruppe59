c63ff14df9739e36195d8d91d8bec3d1faaf3aca
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-02-22 19:08:30 +0100

    feat: move properties to profile

--- activate/usermanagement/models.py
+++ activate/usermanagement/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
