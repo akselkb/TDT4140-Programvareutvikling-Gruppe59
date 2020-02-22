977e818a53a42e1d19d050ff7504a8dfc558d3c0
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-02-22 18:37:10 +0100

    feat: update profile template to tempororay manage profile changes

--- activate/usermanagement/views.py
+++ activate/usermanagement/views.py
@@ -48,14 +48,24 @@ def view_signup(request):
 
 @login_required
 def view_profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('view_profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
 
-    return render(request, 'usermanagement/profile.html')
+    return render(request, 'usermanagement/profile.html', context)
 
 
