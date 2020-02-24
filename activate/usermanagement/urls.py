6044e9bf62c4bd70748d8de245c1ec2f6c912c2d
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-02-25 00:30:35 +0100

    feat: create final template(s) for profile

--- activate/usermanagement/urls.py
+++ activate/usermanagement/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.view_login, name='view_login'),
     path('logout/', views.view_logout, name='view_logout'),
     path('signup/', views.view_signup, name='view_signup'),
-    path('profile/', views.view_profile, name='view_profile')
+    path('profile/', views.view_profile, name='view_profile'),
+    path('modify_profile', views.view_modify_profile, name='view_modify_profile')
 ]
