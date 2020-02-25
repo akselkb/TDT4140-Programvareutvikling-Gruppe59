8253322f58c9e1d3da61ce0796fe208453f96da1
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-02-25 16:18:13 +0100

    feat: add crispy forms to profile(s) template(s)

--- activate/usermanagement/forms.py
+++ activate/usermanagement/forms.py
@@ -22,12 +22,12 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=30, help_text='Valgfri.')
-    last_name = forms.CharField(max_length=30, help_text='Valgfri.')
+    first_name = forms.CharField(max_length=30, help_text='Valgfri.', required=False)
+    last_name = forms.CharField(max_length=30, help_text='Valgfri.', required=False)
     birth_date = forms.DateField(label='Fødselsdato',
                                  widget=forms.SelectDateWidget
                                  (years=[x for x in range(1970, datetime.datetime.now().year+1)]),
-                                 help_text='Valgfri.')
+                                 help_text='Valgfri.', required=False)
 
     class Meta:
         model = Profile
