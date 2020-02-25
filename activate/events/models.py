4fae4ff7ffc8f3a9ce51dbf139dedd9d26a2d034
Markus Hoff Skudal <Markus@dhcp-10-22-15-63.wlan.ntnu.no>
2020-02-25 15:20:08 +0100

    temp: register participants

--- activate/events/models.py
+++ activate/events/models.py
@@ -11,14 +11,15 @@ class Activity(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
 
     # CASCADE: If the user gets deleted, the corresponding activities will also be deleted
-    responsible = models.ForeignKey(User, on_delete=models.CASCADE)
+    responsible = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(app_label)s_%(class)s_responsible')
 
     date_from = models.DateTimeField(verbose_name="fra dato")
     date_to = models.DateTimeField(verbose_name="til dato")
 
     gear = models.CharField(max_length=200, verbose_name="utstyr", default='')
 
-    # registered_users = models.ManyToManyField(User, blank=True, verbose_name='påmeldte brukere')
+    registered_users = models.ManyToManyField(User, blank=True, verbose_name='påmeldte brukere',
+                                              related_name='%(app_label)s_%(class)s_registered')
 
     def __str__(self):
         return self.title
--- activate/events/urls.py
+++ activate/events/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
@@ -5,4 +6,5 @@ urlpatterns = [
     path('', views.activity_list, name='activity_list'),
     path('create/', views.create_activity, name='create_activity'),
     path('<int:id>/', views.activity_detail_view, name='activity_detail_view'),
+    #url(r'^register/(\d+)/$', views.register, name='register'),
 ]
