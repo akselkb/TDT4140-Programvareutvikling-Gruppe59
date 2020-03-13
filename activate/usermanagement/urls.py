66cbf70517d21b2066b90cd34ad8a2d01a14715b
Oda Omholt <oda.omh@gmail.com>
2020-03-13 17:52:57 +0100

    chore: update urls with app names

--- activate/usermanagement/urls.py
+++ activate/usermanagement/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'usermanagement'
+
 urlpatterns = [
     path('login/', views.view_login, name='login'),
     path('logout/', views.view_logout, name='logout'),
