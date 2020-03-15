cc7df2f0ef1c5472fd0f1e20eb31cf401feb9c9c
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-03-15 18:40:49 +0100

    refactor: remove unused imports

--- activate/usermanagement/views.py
+++ activate/usermanagement/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
 
