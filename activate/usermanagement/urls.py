b8a0443e8a16b59256d45fbb6ef6b62fae7d2c76
Aksel K. Borgmo <akselkb@stud.ntnu.no>
2020-03-15 14:55:31 +0100

    refactor: change login/logout views to default built-ins

--- activate/usermanagement/urls.py
+++ activate/usermanagement/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from activate.settings import LOGOUT_REDIRECT_URL as LOGOUT
 from . import views
 
 app_name = 'usermanagement'
 
 urlpatterns = [
-    path('login/', views.view_login, name='login'),
-    path('logout/', views.view_logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='usermanagement/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page=LOGOUT), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
     path('modify-profile/', views.modify_profile, name='modify_profile')
--- activate/usermanagement/views.py
+++ activate/usermanagement/views.py
@@ -4,28 +4,6 @@ from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
 
 
-# Create your views here.
-def view_login(request):
-    # Check if the user just wants to see the login page or is trying to login
-    if request.method == 'POST':
-        # The user has sent a login-form
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None and user.is_active:
-            login(request, user)
-            return redirect('/')
-
-    return render(request, 'usermanagement/login_page.html')
-
-
-def view_logout(request):
-    logout(request)
-    response = redirect('/login/')
-    return response
-
-
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
