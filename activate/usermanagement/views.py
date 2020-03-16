5b0f0a1a0cf252197eb29bdc60966cbb449ecdab
Oda Omholt <oda.omh@gmail.com>
2020-03-16 08:14:45 +0100

    fix: add usermanagement in front of URLs

--- activate/usermanagement/views.py
+++ activate/usermanagement/views.py
@@ -28,7 +28,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            return redirect('profile')
+            return redirect('usermanagement:profile')
 
     else:
         u_form = UserUpdateForm(instance=request.user)
